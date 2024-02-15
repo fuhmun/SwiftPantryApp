3f57c77d44b14b41f8fbeb3d79bf6c36f1b1a0f1
fahadmunawar <fahadshaikh006@gmail.com>
2024-02-15 10:30:52 -0500

    Initial Commit

---
+++ SwiftPantryApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftPantryApp
+//
+//  Created by Fahad Munawar on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftPantryApp/SwiftPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPantryAppApp.swift
+//  SwiftPantryApp
+//
+//  Created by Fahad Munawar on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPantryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
