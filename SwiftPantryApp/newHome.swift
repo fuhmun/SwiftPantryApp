e3a14c530ada7f747e4c1198c2176a024e79c2db
Cannon Goldsby <cannon.goldsby2002@gmail.com>
2024-02-23 10:13:01 -0500

    loading screen

--- SwiftPantryApp/newHome.swift
+++ SwiftPantryApp/newHome.swift
@@ -53,6 +53,8 @@ struct newHome: View {
     @State private var search: String = ""
     @State private var ingredientListed: [String] = []
     
+    @State private var load: Bool = false
+    
     @State private var generatedRecipe: Recipes = Recipes(name: "", time: "", ingredients: "", instructions: "")
     
     @State private var isRecipeHidden: Bool = true
@@ -65,6 +67,7 @@ struct newHome: View {
     let columns = [GridItem(.fixed(100)), GridItem(.fixed(100)), GridItem(.fixed(100))]
     
     @Environment(\.colorScheme) var colorScheme
+    
     
     //    @StateObject var tagModel = TagListViewModel()
     
@@ -165,6 +168,9 @@ struct newHome: View {
                                         }
                                         let ingredientString = ingredientsArray.joined(separator: " ")
                                         print(ingredientString)
+//                                        if load {
+//                                            LoadingScreen()
+//                                        }
                                         Task {
                                             do {
                                                 let result = try await OpenAIService.shared.sendPromptToChatGPT(message: ingredientString)
